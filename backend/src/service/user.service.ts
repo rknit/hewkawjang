4ecfa42615fd6d5671d1b459911529ffcfbf21e1
Chayakorn Unhasirikul <chayakorn26082547@gmail.com>
2025-09-07 12:22:29 +0700

    registration api (#5)
    * add password
    
    * password encryption
    
    * add checking password when login
    
    * fixing error message

--- backend/src/service/user.service.ts
+++ backend/src/service/user.service.ts
@@ -8,6 +8,7 @@ import {
 import { usersTable } from '../db/schema';
 import { db } from '../db';
 import createHttpError from 'http-errors';
+import { hashPassword, comparePassword } from "../utils/hash";
 
 export type User = InferSelectModel<typeof usersTable>;
 export type NewUser = InferInsertModel<typeof usersTable>;
@@ -41,8 +42,22 @@ export default class UserService {
     if (dup.length > 0) {
       throw createHttpError.Conflict('Email already exists');
     }
-
+    data.password = await hashPassword(data.password);
     let [newUser] = await db.insert(usersTable).values(data).returning();
     return newUser;
+  }
+  static async loginUser(data: User): Promise<User> {
+    let loginUser = await db
+      .select()
+      .from(usersTable)
+      .where(eq(usersTable.email, data.email));
+    if (loginUser.length === 0) {
+      throw createHttpError.Unauthorized('Invalid email or password');
+    }
+    const isMatch = await comparePassword(data.password, loginUser[0].password);
+    if (!isMatch) {
+      throw createHttpError.Unauthorized('Invalid email or password');
+    }
+    return loginUser[0];
   }
 }
