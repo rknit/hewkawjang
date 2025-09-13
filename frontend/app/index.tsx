af427095603c8a06a57fb99ba15f49ef91ac0f25
rknit <rachatakanit@gmail.com>
2025-09-14 05:52:21 +0700

    remove tmp profile

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,12 +1,10 @@
 import { logout } from '@/apis/auth.api';
-import { router } from 'expo-router';
-import { Button, Text, View } from 'react-native';
+import { Button, View } from 'react-native';
 
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white gap-8">
       {/* FIXME: for testing purpose */}
-      <Button title="Go to Profile" onPress={() => router.push('/profile')} />
       <Button title="Log Out" onPress={logout} />
     </View>
   );
