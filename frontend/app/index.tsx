04485bba17a238be0a3ffae107d2262abe9489b7
SaritPasiphol <156805144+SaritPasiphol@users.noreply.github.com>
2025-09-15 02:05:36 +0700

    add my-wallet component

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,11 +1,14 @@
-import ReservationTable from '@/components/reservation-table';
-import React from 'react';
+import MyWallet from '@/components/my-wallet';
 import { View } from 'react-native';
 
-export default function RestaurantDashboard() {
+export default function Index() {
+  const handleAddBalance = () => {
+    console.log('Add Balance clicked!');
+  };
+
   return (
-    <View className="flex-1 bg-gray-100 p-4">
-      <ReservationTable />
+    <View className="flex-1 bg-gray-100 p-4 justify-center">
+      <MyWallet balance={5000} onAddBalance={handleAddBalance} />
     </View>
   );
 }
