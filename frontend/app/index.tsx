084054c96c1bfef1593143eae502aac1bfcc3ac4
SaritPasiphol <156805144+SaritPasiphol@users.noreply.github.com>
2025-09-15 00:51:00 +0700

    Additional frontend components (#47)
    * search-panel
    
    * reservation-table

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,11 +1,11 @@
-import { logout } from '@/apis/auth.api';
-import { Button, View } from 'react-native';
+// index.tsx
+import RestaurantDashboard from '@/components/reservation-table';
+import { View } from 'react-native';
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white gap-8">
-      {/* FIXME: for testing purpose */}
-      
+    <View className="flex-1 bg-gray-100">
+      <RestaurantDashboard />
     </View>
   );
 }
