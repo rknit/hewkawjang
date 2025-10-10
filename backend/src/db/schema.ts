225e34c0923099cac4ecc1099422c48960b044a7
Chayakorn Unhasirikul <chayakorn26082547@gmail.com>
2025-10-10 13:22:35 +0700

    fix schema to able to attach photo

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -141,5 +141,6 @@ export const reviewTable = pgTable('review', {
     .unique(),
   rating: integer('rating').notNull(),
   comment: text('comment'),
+  attachPhotos: text('attach_photos'), // URL to the photos
   createdAt: timestamp('created_at').notNull().defaultNow(),
 });
