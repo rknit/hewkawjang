b41e73394b28c8ea54b209c10c4daee1d49c1164
Chayakorn Unhasirikul <chayakorn26082547@gmail.com>
2025-10-12 19:43:22 +0700

    add write review frontend and integrate it with backend

--- frontend/apis/user.api.ts
+++ frontend/apis/user.api.ts
@@ -20,3 +20,19 @@ export async function deleteCurrentUser(): Promise<boolean> {
     return false;
   }
 }
+
+export async function submitReview(
+  reservationId: number,
+  review: { rating: number; attachPhotos: string[]; comment: string })
+: Promise<boolean> {
+  try {
+    await ApiService.post('/users/me/reviews', {
+      reservationId,
+      ...review,
+    });
+    return true;
+  } catch (error) {
+    normalizeError(error);
+    return false;
+  }
+}
