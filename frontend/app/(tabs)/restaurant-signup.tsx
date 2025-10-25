6aa68f7b46a2dfdc7a42d7f883e54c97565d2f34
Poonyapat9 <6632135121@student.chula.ac.th>
2025-10-25 21:34:24 +0700

    US1-8 create restaurant reg form (#88)
    Co-authored-by: Turquoise24 <poonyapatjp@gmail.com>

--- frontend/app/(tabs)/restaurant-signup.tsx
+++ frontend/app/(tabs)/restaurant-signup.tsx
@@ -4,13 +4,9 @@ import BecomeOurPartner from '@/components/become-our-partner';
 
 export default function RestaurantSignUp() {
   return (
-    <View className="flex-row max-h-screen bg-white">
-      <View className="w-2/5 flex-1 pt-48 pl-16">
-        <BecomeOurPartner />
-      </View>
-      <View className="w-3/5 flex-1 pt-8 pr-48">
-        <RegisterRestaurantForm />
-      </View>
+    <View className="flex-row h-screen">
+      <BecomeOurPartner />
+      <RegisterRestaurantForm />
     </View>
   );
 }
