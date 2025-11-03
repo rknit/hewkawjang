9732bd6597859ace0bcfb8bfba21ccab67ce2627
RATSAPORN <rodolf8680@gmail.com>
2025-11-03 12:36:55 +0700

    -update schema.ts

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,3 +1,4 @@
+import { sql } from 'drizzle-orm';
 import {
   pgTable,
   serial,
@@ -9,8 +10,7 @@ import {
   boolean,
   uniqueIndex,
   doublePrecision,
-  real,
-  numeric,
+  check,
 } from 'drizzle-orm/pg-core';
 import { number } from 'zod';
 
@@ -21,6 +21,7 @@ export const usersTable = pgTable('users', {
   email: text('email').notNull().unique(),
   phoneNo: text('phone_no').notNull(),
   password: text('password').notNull(),
+  balance: doublePrecision('balance').notNull().default(0),
   displayName: text('display_name'),
   profileUrl: text('profile_url'),
   refreshToken: text('refresh_token'),
@@ -67,6 +68,11 @@ export const cuisineTypeEnum = pgEnum('cuisine_type', [
   'Italian',
   'Other',
 ]);
+export const paymentMethodEnum = pgEnum(
+  'method',
+
+  ['card', 'HewKawJangwallet', 'PromtPay'],
+);
 
 export const restaurantTable = pgTable('restaurant', {
   id: serial('id').primaryKey(),
@@ -77,11 +83,8 @@ export const restaurantTable = pgTable('restaurant', {
   phoneNo: text('phone_no').notNull(),
   wallet: doublePrecision('wallet').notNull().default(0.0),
   // address
-  houseNo: text('house_no'),
-  village: text('village'),
-  building: text('building'),
-  road: text('road'),
-  soi: text('soi'),
+  address: text('address').notNull(),
+
   subDistrict: text('sub_district'),
   district: text('district'),
   province: text('province'),
@@ -89,10 +92,14 @@ export const restaurantTable = pgTable('restaurant', {
   // detail
   cuisineType: cuisineTypeEnum('cuisine').notNull().default('Other'),
   priceRange: integer('priceRange'),
+  paymentMethod: paymentMethodEnum('payment_method')
+    .notNull()
+    .default('HewKawJangwallet'),
   status: restaurantStatusEnum('status').notNull().default('closed'), // open/close daily ops
   activation: restaurantActivationEnum('activation')
     .notNull()
     .default('active'), // activate/deactivate restaurant
+  isVerified: boolean('is_verified').notNull().default(false),
   isDeleted: boolean('is_deleted').notNull().default(false),
 });
 
@@ -167,6 +174,7 @@ export const reviewTable = pgTable('review', {
   comment: text('comment'),
   attachPhotos: text('attach_photos').array(), // URL to the photos
   createdAt: timestamp('created_at').notNull().defaultNow(),
+  isDeleted: boolean('is_deleted').notNull().default(false),
 });
 
 export const notificationTypesEnum = pgEnum('notification_type', [
@@ -195,6 +203,129 @@ export const withdrawsTable = pgTable('withdraw', {
   restaurantId: integer('restaurant_id')
     .notNull()
     .references(() => restaurantTable.id),
-  balance: doublePrecision('balance').notNull(),
+  amount: doublePrecision('amount').notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+});
+
+export const topupsTable = pgTable('topup', {
+  id: serial('id').primaryKey(),
+  userId: integer('user_id')
+    .notNull()
+    .references(() => usersTable.id),
+  amount: doublePrecision('amount').notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+});
+
+export const adminsTable = pgTable('admin', {
+  id: serial('id').primaryKey(),
+  firstName: text('first_name').notNull(),
+  lastName: text('last_name').notNull(),
+  email: text('email').notNull().unique(),
+  phoneNo: text('phone_no').notNull(),
+  password: text('password').notNull(),
+  displayName: text('display_name'),
+  profileUrl: text('profile_url'),
+  refreshToken: text('refresh_token'),
+  isDeleted: boolean('is_deleted').notNull().default(false),
+});
+
+export const reportTypeEnum = pgEnum('type', [
+  'chat',
+  'user',
+  'review',
+  'restaurant',
+  'support',
+]);
+
+export const chatsTable = pgTable('chat', {
+  id: serial('id').primaryKey(),
+  userId: integer('user_id')
+    .notNull()
+    .references(() => usersTable.id),
+  restaurantId: integer('restaurant_id')
+    .notNull()
+    .references(() => restaurantTable.id),
+  isBanned: boolean('is_banned').notNull().default(false),
+});
+
+export const chatAdminsTable = pgTable('chatadmin', {
+  id: serial('id').primaryKey(),
+  userId: integer('user_id')
+    .notNull()
+    .references(() => usersTable.id),
+  adminId: integer('user_id')
+    .notNull()
+    .references(() => adminsTable.id),
+});
+
+export const messagesTable = pgTable(
+  'message',
+  {
+    id: serial('id').primaryKey(),
+    chatId: integer('chat_id').references(() => chatsTable.id),
+    chatAdminId: integer('chat_admin_id').references(() => chatAdminsTable.id),
+    text: text('text'),
+    imgURL: text('img_url'),
+  },
+  (table) => ({
+    // Ensure exactly one of chatId or chatAdminId is set
+    checkEitherChatOrAdmin: check(
+      'check_either_chat_or_admin',
+      sql`(
+      (${table.chatId} IS NOT NULL AND ${table.chatAdminId} IS NULL) OR
+      (${table.chatId} IS NULL AND ${table.chatAdminId} IS NOT NULL)
+    )`,
+    ),
+  }),
+);
+
+export const imagesTable = pgTable(
+  'image',
+  {
+    id: serial('id').primaryKey(),
+    // Foreign key to Message (attached to messages)
+    messageId: integer('message_id').references(() => messagesTable.id),
+    // Foreign key to User (profile picture)
+    userProfileId: integer('user_profile_id').references(() => usersTable.id),
+    // Foreign key to Restaurant (profile picture)
+    restaurantProfileId: integer('restaurant_profile_id').references(
+      () => restaurantTable.id,
+    ),
+    // Foreign key to Review (attached to reviews)
+    reviewId: integer('review_id').references(() => reviewTable.id),
+    // Image data/metadata
+    url: text('url').notNull(),
+    fileName: text('file_name'),
+    fileSize: integer('file_size'),
+    mimeType: text('mime_type'),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+  },
+  (table) => ({
+    // Ensure the image belongs to exactly one entity
+    checkSingleReference: check(
+      'check_single_reference',
+      sql`(
+      (${table.messageId} IS NOT NULL AND ${table.userProfileId} IS NULL AND ${table.restaurantProfileId} IS NULL AND ${table.reviewId} IS NULL) OR
+      (${table.messageId} IS NULL AND ${table.userProfileId} IS NOT NULL AND ${table.restaurantProfileId} IS NULL AND ${table.reviewId} IS NULL) OR
+      (${table.messageId} IS NULL AND ${table.userProfileId} IS NULL AND ${table.restaurantProfileId} IS NOT NULL AND ${table.reviewId} IS NULL) OR
+      (${table.messageId} IS NULL AND ${table.userProfileId} IS NULL AND ${table.restaurantProfileId} IS NULL AND ${table.reviewId} IS NOT NULL)
+    )`,
+    ),
+  }),
+);
+
+export const reportsTable = pgTable('report', {
+  id: serial('id').primaryKey(),
+  userId: integer('user_id')
+    .notNull()
+    .references(() => usersTable.id),
+  adminId: integer('user_id').references(() => adminsTable.id),
+  targetRestaurantId: integer('restaurant_id').references(
+    () => restaurantTable.id,
+  ),
+  targetReviewId: integer('review_id').references(() => reviewTable.id),
+  targetUserId: integer('taget_user_id').references(() => usersTable.id),
+  targetChatId: integer('chat_id').references(() => chatsTable.id),
+  isSolved: boolean('is_solved').notNull().default(false),
   createdAt: timestamp('created_at').notNull().defaultNow(),
 });
