77151f0dfb266cf3376f5a3d5630557442dbdc2d
rknit <rachatakanit@gmail.com>
2025-11-17 15:20:04 +0700

    test: add more playwright test

--- frontend/tests/Createreview.spec.ts
+++ frontend/tests/Createreview.spec.ts
@@ -1,6 +1,6 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
 
-test('WriteReview', async ({ page, context }) => {
+async function setup(page: Page) {
   // Set authentication token in localStorage and cookie before navigating
   await page.goto('/');
   await page.getByText('Login').click();
@@ -11,9 +11,30 @@ test('WriteReview', async ({ page, context }) => {
   await page.getByRole('textbox', { name: 'Enter your password' }).click();
   await page.getByRole('textbox', { name: 'Enter your password' }).fill('test');
   await page.getByText('Log in').click();
+
   await page.getByText('My Reservation').click();
   await page.getByText('Completed').click();
   await page.getByText('Give Rating').first().click();
+}
+
+test('WriteReviewInvalid', async ({ page, context }) => {
+  await setup(page);
+
+  // Submit without selecting rating or entering text
+  page.once('dialog', async (dialog) => {
+    expect(dialog.message()).toBe('Please select a rating.');
+    await dialog.accept();
+  });
+  await page
+    .locator('div')
+    .filter({ hasText: /^Submit$/ })
+    .first()
+    .click();
+});
+
+test('WriteReviewValid', async ({ page, context }) => {
+  await setup(page);
+
   await page.getByRole('img').nth(3).click();
   page.once('dialog', async (dialog) => {
     expect(dialog.message()).toBe('Review submitted successfully!');
